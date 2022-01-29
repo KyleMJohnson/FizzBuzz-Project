bfbb341394a7aa1f7918ab68224114d1b747d197
Kyle Johnson <kylemjohnson477@gmail.com>
2022-01-28 22:53:32 -0800

    Added explanation comments

--- FizzBuzz C++/Source.cpp
+++ FizzBuzz C++/Source.cpp
@@ -5,23 +5,29 @@
 
 int main()
 {
+	//initiate loop and iterate from 1 to 100
 	for (int i = 1; i < 101; i++)
 	{
+		//check if # is divisible by 3
 		if (i % 3 == 0)
 		{
+			//check if # is also divisible by 5
 			if (i % 5 == 0)
 			{
 				std::cout << "FizzBuzz\n";
 			}
+			//only divisible by 3
 			else
 			{
 				std::cout << "Fizz\n";
 			}
 		}
+		//only divisible by 5
 		else if (i % 5 == 0)
 		{
 			std::cout << "Buzz\n";
 		}
+		//prints # if not divisible by 3 or 5
 		else
 			std::cout << i << "\n";
 	}
