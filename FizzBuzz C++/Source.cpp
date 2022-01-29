319d63b8966571aa9c349294be9432dc28199da6
Kyle Johnson <kylemjohnson477@gmail.com>
2022-01-28 22:49:57 -0800

    finished code without comments

---
+++ FizzBuzz C++/Source.cpp
@@ -0,0 +1,31 @@
+
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	for (int i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0)
+		{
+			if (i % 5 == 0)
+			{
+				std::cout << "FizzBuzz\n";
+			}
+			else
+			{
+				std::cout << "Fizz\n";
+			}
+		}
+		else if (i % 5 == 0)
+		{
+			std::cout << "Buzz\n";
+		}
+		else
+			std::cout << i << "\n";
+	}
+
+
+
+}
